12292c7d293518192d35ee0681f2d26085eedfad
singhabhinendra <singhabhinendra337@gmail.com>
2024-11-22 16:36:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// // document.getElementById("count-el").innerText="23"
+// // console.log("abhinendra singh")
+// // let count = 5
+// // console.log(count)
+// // console.log(myAge)
+// //
+// // let myAge = 35
+// //
+// // let firstBatch = 5
+// // let secondBatch = 5
+// // let count = firstBatch + secondBatch
+// // console.log(count)
+// /* 1- Create two variables,myAge and humanDogRatio
+// // * 2-Multiply the two together and store the result in myDog*
+// // 3-Log myDogAge to the console
+
+const accountId = 122132;
+let accountEmail = "abhinendras753@gmail.com";
+var accountPassword = "abhinendra@123";
+accountCity = "Etawah"
+// accountId = 2;
+accountEmail = "etawah@gmail.com"
+accountPassword = "etawah@gmail.com"
+console.log(accountEmail);
+console.log(accountPassword);
+accountCity = "Etawah";
+accountEmail = "etawah@gmail.com";
+accountPassword = "etawah@gmail.com";
+console.log(accountCity);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+/*
+Prefer not to user var because of issue in block scope and functional scope
+ */
+
+function abcd() {
+    return "abhinendra";
+}
+
+
+const ans = abcd();
